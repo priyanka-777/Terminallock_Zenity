55a04471292f96a16f82079895ad4e72dbe97878
priyanka-777 <o180307@rguktong.ac.in>
2023-02-28 19:37:00 +0530

    added files for lock

---
+++ index.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#Checking Zenity is installed or not 
+which zenity > /dev/null
+if [ $? == 0 ]
+then
+echo "Zenity already installed"
+else
+ping -c 3 google.com
+if [ $? == 0 ]
+then 
+sudo apt-get update -y
+sudo apt-get install -y zenity
+else
+echo "Check your Internet Connection"
+fi
+fi
+#Checking Figlet tool is present or not
+which figlet > /dev/null
+if [ $? == 0 ]
+then 
+echo "Figlet already Installed"
+else
+ping -c 3 google.com
+if [ $? == 0 ]
+then
+sudo apt-get update -y
+sudo apt-get install figlet
+else
+echo "Check your Internet connection"
+fi
+fi
+#Check md5sum is present or not
+which md5sum > /dev/null
+if [ $? == 0 ]
+then
+echo "md5sum is present"
+else
+ping -c 3 google.com
+if [ $? == 0 ]
+then 
+sudo apt-get update -y
+sudo apt-get install ucommon-utils
+else
+echo "Check your Internet Connection"
+fi
+fi
+password=$(zenity --password --title="Set password")
+
+echo
+sed "s/teju/`md5sum <<< $password`/g" terminallock.sh > TerminalLock
+sudo rm terminallock.sh
+sudo chmod +x TerminalLock
+sudo mv TerminalLock /usr/bin/
+sudo echo TerminalLock >> $HOME/.bashrc
+cd
+sudo rm -r Terminallock_zenity
+
---
+++ terminallock.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo
+password="$(zenity --password --title="Login")"
+
+if [[ `md5sum <<< $password` == "teju" ]]
+then
+figlet "welcome"
+else
+figlet "who are you"
+sleep 3
+kill $(pgrep terminal)
+fi
+
